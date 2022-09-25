8f74344b3df504e5648d3f2784ce93d33ee8c980
Lucian Lodico <llodico@mac.com>
2022-09-25 18:03:39 -0400

    draft

--- maincustdata.js
+++ maincustdata.js
@@ -1,7 +1,4 @@
-//const moment = require("./moment");
-
 const customerContainer = document.querySelector("#customerContain")
-// 
 
 function buildDirectory(customerArray){
     for (let customer of customerArray) {
@@ -14,7 +11,7 @@ function buildDirectory(customerArray){
         } 
 
         let name = document.createElement("p"); 
-        name.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
+        name.innerText = `${capitalizeFirstLetter(customer.name.title)} ${customer.name.first} ${customer.name.last}`;
 
         let location = document.createElement("p"); 
         location.innerText = `${customer.location.city} ${customer.location.state} ${customer.location.country} ${customer.location.postcode}`; 
@@ -22,13 +19,50 @@ function buildDirectory(customerArray){
         let email = document.createElement("p"); 
         email.innerText = `${customer.email}`; 
 
+        let birth = document.createElement("p"); 
+        birth.innerText = `${moment(customer.dob.date).format("MMM Do YYYY")}`;
+
+        let large = document.createElement("img"); 
+        large.src = `${customer.picture.large}`; 
+
+        let phone = document.createElement("p") 
+        phone.innerText = `${customer.phone}`; 
+   
         customerDiv.appendChild(name); 
         customerDiv.appendChild(location); 
         customerDiv.appendChild(email); 
-    
+        customerDiv.appendChild(birth); 
+        customerDiv.appendChild(large); 
+        customerDiv.appendChild(phone); 
 
         customerContainer.appendChild(customerDiv); 
     }
 } 
 
 buildDirectory(customers)
+
+
+  function hidePeople(container) {
+    let personDivs = container.querySelectorAll(".customer");
+    for (let div of personDivs) {
+      container.removeChild(div);
+    }}
+
+    function showPeople(container) {
+        let personDivs = container.querySelectorAll(".customer"); 
+        for (let div of personDivs) {
+            container.addChild(div);
+        }
+      }
+
+  const showMeButton = document.querySelector("#test")
+  
+  showMeButton.addEventListener("click", (event) => {
+    if (showMeButton.innerText === "Pop the peepz!") {
+      hidePeople(customerContainer);
+      showMeButton.innerText = "Peep ye peeps!";
+    } else {
+      showPeople(customerContainer);
+      showMeButton.innerText = "Pop the Peepz!";
+    }
+  })
