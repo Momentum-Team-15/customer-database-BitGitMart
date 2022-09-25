11df42f773fbbd2e43d2d9c33c5ccb2158cd4971
Lucian Lodico <llodico@mac.com>
2022-09-25 18:39:09 -0400

    Working Draft

--- maincustdata.js
+++ maincustdata.js
@@ -4,39 +4,50 @@ function buildDirectory(customerArray){
     for (let customer of customerArray) {
         let customerDiv = document.createElement("div"); 
         customerDiv.classList.add("customer"); 
-    
+
+        function capitalize(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+          }
 
         let name = document.createElement("p"); 
-        name.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
+        name.innerText = `${capitalize(customer.name.first)} ${capitalize(customer.name.last)}`;
+
+        let street = document.createElement("p")
+        street.innerText = `Address: ${customer.location.street.number} ${customer.location.street.name},`; 
 
         let location = document.createElement("p"); 
-        location.innerText = `${customer.location.city} ${customer.location.state} ${customer.location.country} ${customer.location.postcode}`; 
+        location.innerText = `${customer.location.city}, ${nameToAbbr(customer.location.state)} ${customer.location.postcode}`; 
 
         let email = document.createElement("p"); 
-        email.innerText = `${customer.email}`; 
+        email.innerText = `Email: ${customer.email}`; 
 
         let birth = document.createElement("p"); 
-        birth.innerText = `${moment(customer.dob.date).format("MMM Do YYYY")}`;
+        birth.innerText = `DOB: ${moment(customer.dob.date).format("MMM Do YYYY")}`;
 
         let large = document.createElement("img"); 
         large.src = `${customer.picture.large}`; 
 
         let phone = document.createElement("p") 
         phone.innerText = `${customer.phone}`; 
+
+        let registered = document.createElement("p")
+        registered.innerText = `Registered: ${moment(customer.registered.date).format("MMM Do YYYY")}`
    
         customerDiv.appendChild(name); 
+        customerDiv.appendChild(street); 
         customerDiv.appendChild(location); 
         customerDiv.appendChild(email); 
         customerDiv.appendChild(birth); 
         customerDiv.appendChild(large); 
         customerDiv.appendChild(phone); 
+        customerDiv.appendChild(registered); 
 
         customerContainer.appendChild(customerDiv); 
     }
 } 
 
 buildDirectory(customers)
-
+  
 
   function hidePeople(container) {
     let personDivs = container.querySelectorAll(".customer");
